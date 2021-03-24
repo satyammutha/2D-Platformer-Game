1bb346a6a6249e0a5bad644631d205bcac532fb6
satyammutha <satyammutha@gmail.com>
2021-03-24 18:05:31 +0530

    - Chapter 3 Mini Assignment 2 -> Binding Animations with Input, Flipping Character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Animator animator;
     private void Awake()
     {
         Debug.Log("Player Controller Awake");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("IN Collision :" + collision.gameObject.name);
+    //}
+
+    private void Update()
     {
-        Debug.Log("IN Collision :"+collision.gameObject.name);
+        float speed = Input.GetAxisRaw("Horizontal") ;
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector2 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
