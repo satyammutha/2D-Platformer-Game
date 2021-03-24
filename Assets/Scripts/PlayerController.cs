2cc1695348ba4743ec97ab4bb17ef90bdc971399
satyammutha <satyammutha@gmail.com>
2021-03-24 17:09:24 +0530

    - Chapter 3 Mini Assignment 1 -> Player Controller Script, Physics, RigidBody. Collision

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("Player Controller Awake");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("IN Collision :"+collision.gameObject.name);
+    }
+}
