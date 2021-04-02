363c11417b1ad5280e6c2d5821a82cd4ce2f18b2
satyammutha <satyammutha@gmail.com>
2021-04-02 17:06:08 +0530

    - Added Chapter 6 - Mini Assignment  - Collectible (Keys,Score)

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class ScoreController : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+    private int score;
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        RefreshUI();
+    }
+
+    public void IncreaseScore (int increment)
+    {
+        score += increment;
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        scoreText.text = "SCORE: " + score;
+    }
+}
