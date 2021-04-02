4d3f48198723d480389fa174ee03b5c56f8ebbf4
satyammutha <satyammutha@gmail.com>
2021-04-02 23:54:43 +0530

    - Added Solution of Chapter 6  - Challange

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public Animator animator;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
